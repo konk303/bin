9b63062d516b60e361ba202929066c26dd6a7511
Kimihiko NAKAMURA <kimihiko.nakamura.fz@ps.hitachi-solutions.com>
2013-06-18 17:18:53 +0900

    css

--- rewrite_css.rb
+++ rewrite_css.rb
@@ -6,13 +6,13 @@ require 'pry'
 # == cssのurlをimage-url書き換えてファイル名をscss化
 module RewriteCss
   def self.initialize(file_name_prefixes = [])
-    base_dir = "#{Dir.home}/work/front/app/assets/stylesheets/mobile"
+    # base_dir = "#{Dir.home}/work/front/app/assets/stylesheets/mobile"
     # base_dir = "#{Dir.home}/work/front/app/assets/stylesheets/smart_phone"
-    # base_dir = "#{Dir.home}/work/front/app/assets/stylesheets/"
+    base_dir = "#{Dir.home}/work/front/app/assets/stylesheets/"
     @files = [].tap do |files|
       [
         # "basic.css",
-        "common.css",
+        "common.css.scss",
         # "item.css",
         # "shop.css",
         # "top.css",
@@ -31,10 +31,10 @@ module RewriteCss
     @files.each do |file|
       texts = File.read(file)
       # replaced = texts
-      replaced = texts.gsub(%r{url\("/(.*?)"\)}) {"image-url(\"#{$1}\")"}
+      # replaced = texts.gsub(%r{url\("/(.*?)"\)}) {"image-url(\"#{$1}\")"}
       # replaced = texts.gsub(%r{url\(/(.*?)\)}) {"image-url(\"#{$1}\")"}
-      # replaced = texts.gsub(%r{url\(/img/(.*?)\)}) {"image-url(\"#{$1}\")"}
-      File.open("#{file}.scss", "w") {|f| f.puts replaced}
+      replaced = texts.gsub(%r{url\("/img/(.*?)"\)}) {"image-url(\"#{$1}\")"}
+      File.open("#{file}", "w") {|f| f.puts replaced}
     end
     puts "finished"
   end
