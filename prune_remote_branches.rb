6bdd2a887598fafb8d9b7b81eb493fd8c6dfce1a
Kimihiko NAKAMURA <kimihiko.nakamura.fz@ps.hitachi-solutions.com>
2013-06-21 17:19:59 +0900

    prune remote repo and git gc

---
+++ prune_remote_branches.rb
@@ -0,0 +1,46 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# = originから、releaseにマージ済みのbranchを削除
+module PruneRemoteBranches
+  def self.initialize
+    @wd = "#{Dir.home}/tmp/git"
+    @repos = [:front, :back, :batch, :lws_framework]
+  end
+
+  def self.execute
+    initialize
+    @repos.each do |repo|
+      d = File.expand_path(repo.to_s, @wd)
+      # ログを綺麗にみたいので、thread止める
+      # threads << Thread.start(path, branches) do |d, branches|
+      unless `cd #{d} && git status`.match("working directory clean")
+        raise "#{d} not clean, no pull this time"
+      end
+
+      # fetch
+      run_command("cd #{d} && git fetch")
+      # find branches that already merged to release
+      deletes = `cd #{d} && git branch -r --merged origin/release`.split("\n").
+        grep(%r{^\s*origin/[hf]/}).map{|b| b.sub(%r{^\s*origin/}, "")}
+      puts "  delete #{deletes.size} remote branches"
+      deletes.each do |branch|
+        # puts "cd #{d} && git push origin :#{branch}"
+        run_command("cd #{d} && git push origin :#{branch}")
+      end
+      # fetch again and gc
+      run_command("cd #{d} && git fetch")
+      run_command("cd #{d} && git gc")
+      # also gc work/
+      run_command("cd ~/work/#{repo} && git fetch")
+      run_command("cd ~/work/#{repo} && git gc")
+    end
+  end
+
+  def self.run_command(command)
+    puts "execute: #{command}"
+    raise unless system(command)
+  end
+end
+
+PruneRemoteBranches.execute
